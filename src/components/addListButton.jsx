1152aeaa86d19d149f5ef50bf2d0677de4241c3d
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-01 01:21:55 -0800

    resize button

--- src/components/addListButton.jsx
+++ src/components/addListButton.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function addList() {
   return (
-    <button type='button' className='btn btn-primary rounded-2xl w-72 flex justify-start font-bold text-base'>+ Add List</button>
+    <button type='button' className='btn btn-sm btn-primary rounded-2xl w-64 flex justify-start font-bold text-base'>+ Add List</button>
   )
 }
