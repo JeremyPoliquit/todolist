e7adb1dc32a25e76ee011cf10c83f9e55540ce79
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-01 01:40:06 -0800

    new input form implement

---
+++ src/components/inputForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function inputForm() {
+  return (
+    <div>
+      <input type="text" className='border px-4 py-2 bg-white rounded-xl shadow-md w-full' placeholder='Type something' />
+    </div>
+  )
+}
