a224a180144b9d487f92e1dd89061ca14c78d473
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-11-30 21:10:23 -0800

    Button for adding list

---
+++ src/components/addListButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function addList() {
+  return (
+    <button type='button' className='btn btn-primary rounded-2xl w-72 flex justify-start font-bold text-base'>+ Add List</button>
+  )
+}
