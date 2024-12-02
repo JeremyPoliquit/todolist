02a5e2fb09555bc56d85fc0aa20656b448c207ea
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-02 02:49:27 -0800

    Testing if its works my toggle button open and close

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import AddButtonList from "../components/addListButton";
-import List from "../components/List";
+import ButtonCard from "../components/Button";
+import CardForm from "../components/CardItem/createCard";
+import InputTitle from "../components/ListItem/inputTitle"
+import useOpen from "../hooks/useOpen";
+import useClose from "../hooks/useClose";
+import useToggle from "../hooks/useToggle"
 
 function Home() {
+  const { openVisible, open } = useOpen();
+  const { closeVisible, close } = useClose();
+  const { isVisible, toggleVisiblity} = useToggle()
+
   return (
     <div className="min-h-screen h-screen flex flex-col">
       <div className="h-16 flex-none">
         <Navbar title="Todo List" />
       </div>
 
-      <div className="flex gap-4 mx-7 mt-5 w-full max-w-screen-xl h-full max-h-full overflow-x-auto">
-        <AddButtonList />
-        <List />
-        <AddButtonList />
-        <List />
-        <AddButtonList />
+      <div className="flex gap-4 mx-7 mt-5 w-full max-w-screen-xl  overflow-x-auto">
+        { (!openVisible && <ButtonCard onClick={open} /> ) || <CardForm /> || (!isVisible && <CardForm onClick={toggleVisiblity} />) || <ButtonCard/> }
       </div>
     </div>
   );
