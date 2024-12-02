afa5479ffaafaf03dcd1a96734a0610cf9ec92e1
JeremyPoliquit <jeremypoliquit.student@gmail.com>
2024-12-02 02:55:27 -0800

    idk why i put props label

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Navbar({title}) {
   return (
-    <nav className='container flex justify-start items-center max-w-full py-4 px-4 bg-slate-700 text-white'>
-        <h1 className='font-semibold text-xl'>{title}</h1>
+    <nav className='container flex justify-start items-center max-w-full py-4 px-4 mb-5 bg-slate-700 text-white'>
+        <h1 className='font-semibold text-xl'>Todo List</h1>
     </nav>
   )
 }
